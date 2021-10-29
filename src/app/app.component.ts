2934657b7571e3f094ebd66c7f233a7a457924a6
Anas Al Kala <aal1580@students.fhv.at>
2021-10-29 13:22:59 +0200

    fixing bug of undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name: string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  miles: number | undefined;
 
-  onNameChange(event: Event){
-    const target=event.target as HTMLInputElement;
-    const value=target.value;
-    this.name=value;
+  car={
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  };
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.name = value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.date = value;
+  }
+
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.miles = parseFloat(value);
   }
 }
